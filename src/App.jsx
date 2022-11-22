161177de62acbdcbfea8ae4350a34dc4cbaa86f9
JuanDeossa <juandeossa.nc@gmail.com>
2022-11-22 10:55:17 -0500

    Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
+import { Button } from './components/Button/Button'
 
 export function App() {
   return (
     <div className="App">
-
+      <Button text="Student"/>
     </div>
   )
 }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+
+import './style/index.css';
+
+export const Button = (props) => {
+    const {text}=props
+    return (
+        <button>{text}</button>
+    );
+};
+

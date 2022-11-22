2ccc31abe9086e5fe74495cd17110598398d4f40
JuanDeossa <juandeossa.nc@gmail.com>
2022-11-22 16:50:16 -0500

    configuration lists

--- src/pages/StudentConfig/StudentConfig.jsx
+++ src/pages/StudentConfig/StudentConfig.jsx
@@ -1,9 +1,36 @@
-
-import { TemplateComp } from '../../components/TemplateComp/TemplateComp';
-import './style/index.css';
+import { TemplateComp } from "../../components/TemplateComp/TemplateComp";
+import "./style/index.css";
 
 export const StudentConfig = (props) => {
-    return (
-        <TemplateComp text="student config"/>
-    );
-}
+  return (
+    <div>
+      <TemplateComp text="Student config" />
+      <form action="">
+      <div className="option-container">
+        <label htmlFor="">Category</label>
+        <select name="category">
+          <option value="value1">Value 1</option>
+          <option value="value2">Value 2</option>
+          <option value="value3">Value 3</option>
+        </select>
+      </div>
+      <div className="option-container">
+        <label htmlFor="">Difficulty</label>
+        <select name="dificulty">
+          <option value="value1">Value 1</option>
+          <option value="value2">Value 2</option>
+          <option value="value3">Value 3</option>
+        </select>
+      </div>
+      <div className="option-container">
+        <label htmlFor="">Type</label>
+        <select name="category">
+          <option value="value1">Value 1</option>
+          <option value="value2">Value 2</option>
+          <option value="value3">Value 3</option>
+        </select>
+      </div>
+      </form>
+    </div>
+  );
+};

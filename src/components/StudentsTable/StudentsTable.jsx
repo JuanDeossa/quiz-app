0c2280061602047e19f874a241a47f2a41dd4f88
JuanDeossa <juandeossa.nc@gmail.com>
2022-12-05 22:48:24 -0500

    professor page styles ok

--- src/components/StudentsTable/StudentsTable.jsx
+++ src/components/StudentsTable/StudentsTable.jsx
@@ -6,45 +6,81 @@ import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { RiDeleteBin6Line } from "react-icons/ri";
+import { RiDeleteBin6Line, RiEyeLine } from "react-icons/ri";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
 
 export const StudentsTable = () => {
   const [studentsDB, setStudentsDB] = useLocalStorage("studentsDB", []);
+
   const deleteRow = (name) => {
-    const newStudentsArray=studentsDB.filter(student=>student.name!==name);
-    setStudentsDB(newStudentsArray)
+    const newStudentsArray = studentsDB.filter(
+      (student) => student.name !== name
+    );
+    setStudentsDB(newStudentsArray);
   };
+
+  const showRowDetails = (name) => {
+    console.log(studentsDB);
+    console.log(name);
+  };
+
+  const cellStyles = {
+    label: {
+      padding: {
+        xs: "10px 0px 10px 10px",
+        sm: "10px 10px",
+      },
+      fontSize: {
+        xs: "16px",
+        sm: "25px",
+      },
+      fontWeight: "900",
+    },
+    value: {
+      padding: {
+        xs: "10px 0px 10px 10px",
+        sm: "10px 10px",
+      },
+      fontSize: {
+        xs: "14px",
+        sm: "16px",
+      },
+    },
+  };
+
+  const labels = ["Name", "Score", "Aproved", "Delete", "Details"];
   return (
-    <div className="table-container">
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 250 }} aria-label="simple table">
+    <div>
+      <TableContainer
+        component={Paper}
+        sx={{
+          width: "95% ",
+          minWidth: "260px ",
+          maxWidth: "550px ",
+          margin: "0 auto",
+        }}
+      >
+        <Table aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell className="table-label" align="left">
-                Name
-              </TableCell>
-              <TableCell className="table-label" align="left">
-                Score
-              </TableCell>
-              <TableCell className="table-label" align="left">
-                Aproved
-              </TableCell>
-              <TableCell className="table-label" align="left"></TableCell>
+              {labels.map((label, i) => (
+                <TableCell
+                  align={label === "Name" ? "left" : "center"}
+                  key={i}
+                  sx={cellStyles.label}
+                >
+                  {label}
+                </TableCell>
+              ))}
             </TableRow>
           </TableHead>
           <TableBody>
             {!studentsDB.length
               ? null
               : studentsDB.map((row, i) => (
-                  <TableRow
-                    key={i}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell className="table-data" align="left">
-                      {row?.name}
-                    </TableCell>
-                    <TableCell className="table-data" align="center">
+                  <TableRow key={i}>
+                    <TableCell sx={cellStyles.value}>{row?.name}</TableCell>
+                    <TableCell align="center" sx={cellStyles.value}>
                       {row?.score}
                     </TableCell>
                     <TableCell
@@ -52,13 +88,25 @@ export const StudentsTable = () => {
                         row?.aproved ? "yes" : "no"
                       }`}
                       align="center"
+                      sx={cellStyles.value}
                     >
                       {row?.aproved ? "Yes" : "No"}
                     </TableCell>
-                    <TableCell className="table-data" align="center">
-                      <div id={row.name} onClick={() => deleteRow(row.name)}>
-                        <RiDeleteBin6Line className="delete-icon" />
-                      </div>
+                    <TableCell
+                      align="center"
+                      sx={cellStyles.value}
+                      id={row.name}
+                      onClick={() => deleteRow(row.name)}
+                    >
+                      <RiDeleteBin6Line className="delete-icon" />
+                    </TableCell>
+                    <TableCell
+                      align="center"
+                      sx={cellStyles.value}
+                      id={row.name}
+                      onClick={() => showRowDetails(row.name)}
+                    >
+                      <RiEyeLine className="details-icon" />
                     </TableCell>
                   </TableRow>
                 ))}
--- src/pages/ProfessorPage/ProfessorPage.jsx
+++ src/pages/ProfessorPage/ProfessorPage.jsx
@@ -1,19 +1,13 @@
 import { StudentsTable } from "../../components/StudentsTable/StudentsTable";
-import { useLocalStorage } from "../../hooks/useLocalStorage";
 import "./style/index.css";
 
-// const students =[
-//   { name: "Juan", score: 100, aproved: true },
-//   { name: "Richard", score: 100, aproved: true },
-//   { name: "Elon", score: 20, aproved: false },
-// ];
-
-export const ProfessorPage = (props) => {
-  // const [studentsDB, setStudentsDB] = useLocalStorage("studentsDB", []);
+export const ProfessorPage = () => {
   return (
     <div className="professor-page">
-      <h3>ProfessorPage</h3>
-      <StudentsTable  />
+      <div className="students-table">
+        <h3>Students</h3>
+        <StudentsTable />
+      </div>
     </div>
   );
 };
